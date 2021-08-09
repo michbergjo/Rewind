9fe850ffcd3b6f3f8687cc6456b40531a5c6e58f
Michelle Joseph <mbergy27@gmail.com>
2021-08-08 19:17:17 -0500

    Added JavaScript to make the app work
    Added JS for functionality, added description of the JS functions, added JS code to the prism code to display

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-// Call Hello World 
-function HelloWorld(){
-    alert("hello world");
+//Get String from the page
+//controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+   let userString = document.getElementById("userString").value;
+
+   let revString = reverseString(userString);
+
+   displayString(revString);
+}
+
+//Reverse String
+//logic function
+function reverseString(userString){
+
+    let revString = [];
+    
+    //reverse a String using a for loop
+    for (let index = userString.length-1; index >= 0; index--) {
+      revString += userString[index];
+        
+    }
+
+    return revString;
+}
+
+//Display reversed String to the user 
+//view function 
+function displayString(revString){
+
+    //write the message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //turn on the alert box 
+    document.getElementById("alert").classList.remove("invisible");
 }
